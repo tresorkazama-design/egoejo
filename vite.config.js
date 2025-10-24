a43269326fe106ab846f4a6da8324bc45627bb7c
Tresor Kazama <tresor.kazama@gmail.com>
2025-10-24 13:44:55 +0200

    init EGOEJO

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 ﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "EGOEJO",
+        short_name: "EGOEJO",
+        theme_color: "#00ffa3",
+        background_color: "#030806",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          { src: "/favicon.svg", sizes: "64x64", type: "image/svg+xml", purpose: "any" }
+        ]
+      }
+    })
+  ]
 });
